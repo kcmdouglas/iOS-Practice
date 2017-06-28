1a71f9ebe65b3169dbfa5ffbb35def07c8fd6bd1
Kassidy M Douglas <kcmdouglas@gmail.com>
2017-06-28 14:56:21 -0700

    Add API call to search iTunes, add new function to check results

--- iOS-Practice/ViewController.swift
+++ iOS-Practice/ViewController.swift
@@ -9,6 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    var tableData = [[String: String]]()
+    @IBOutlet var appsTableView : UITableView!
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 10
     }
@@ -32,7 +35,69 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func searchItunes(searchTerm: String) {
+        // Replace spaces with + to search multiple terms at once
+        let itunesSearchTerm = searchTerm.replacingOccurrences(of: " ", with: "+", options: .caseInsensitive, range: nil)
+        // Also replace every character with a percent encoding
+        let escapedSearchTerm = itunesSearchTerm.addingPercentEncoding(withAllowedCharacters: [])!
+        
+        // This is the URL that Apple offers for their search API
+        let urlString = "http://itunes.apple.com/search?term=\(escapedSearchTerm)&media=software"
+        let url = URL(string: urlString)!
+        URLSession.shared.dataTask(with: url) { (data, response, error) in
+            if let error = error {
+                // If there is an error in the web request, print it to the console
+                print(error)
+                return
+            }
+            // Because we'll be calling a function that can throw an error
+            // we need to wrap the attempt inside of a do { } block
+            // and catch any error that comes back inside the catch block
+            do {
+                let jsonResult = try JSONSerialization.jsonObject(with: data!, options: []) as! [String: Any]
+                self.didReceive(searchResult: jsonResult)
+            }
+            catch let err {
+                print(err.localizedDescription)
+            }
+            // Close the dataTask block, and call resume() in order to make it run immediately
+            }.resume()
+    }
+    
+    func didReceive(searchResult: [String: Any]) {
+        // Make sure the results are in the expected format of [String: Any]
+        guard let results = searchResult["results"] as? [[String: Any]] else {
+            print("Could not process search results...")
+            return
+        }
+        
+        // Create a temporary place to add the new list of app details to
+        var apps = [[String: String]]()
+        
+        // Loop through all the results...
+        for result in results {
+            // Check that we have String values for each key we care about
+            if let thumbnailURLString = result["artworkUrl100"] as? String,
+                let appName = result["trackName"] as? String,
+                let price = result["formattedPrice"] as? String {
+                // All three data points are valid, add the record to the list
+                apps.append(
+                    [
+                        "thumbnailURLString": thumbnailURLString,
+                        "appName": appName,
+                        "price": price
+                    ]
+                )
+            }
+        }
+        // Update our tableData variable as a way to store
+        // the list of app's data we pulled out of the JSON
+        tableData = apps
+        // Refresh the table with the new data
+        DispatchQueue.main.async {
+            self.appsTableView.reloadData()
+        }
+    }
 }
 
